65814c5114e57365fbc8358017359cce300a99e7
DG2028-2029 <diegogironlay@gmail.com>
2026-05-24 20:22:56 -0600

    Update config_clientes.py

--- config_clientes.py
+++ config_clientes.py
@@ -1,16 +1,17 @@
 CLIENTES = {
     "roberto": {
         "nombre": "Roberto Inmobiliaria",
-        "premium_email": False,  
+        "premium_email": False,
+        "email_api_key": "",
         "whatsapp": "50230840598",
         "usuario": "roberto",
         "password": "1234",
         "archivo_csv": "clientes/roberto/leads.csv"
     },
-
     "juan": {
         "nombre": "Juan Realty",
         "premium_email": False,
+        "email_api_key": "",
         "whatsapp": "50231111111",
         "usuario": "juan",
         "password": "5678",
